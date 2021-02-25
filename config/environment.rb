df574234a718adf1f088b9d7910cad27ef0d7856
SuperPox <calebkohl@gmail.com>
2021-02-24 20:39:21 -0700

    commit tree 13be1e1f22b415a6c0a07669881cc8d20da47062

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,11 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
+
 def fi_check_migration
   begin
     ActiveRecord::Migration.check_pending!
+    use Rack::MethodOverride
   rescue ActiveRecord::PendingMigrationError
     raise ActiveRecord::PendingMigrationError.new "Migrations are pending.\nTo resolve this issue, run: \nrake db:migrate SINATRA_ENV=#{ENV['SINATRA_ENV']}"
   end
@@ -17,3 +19,4 @@ ActiveRecord::Base.establish_connection(
 )
 
 require_all 'app'
+
